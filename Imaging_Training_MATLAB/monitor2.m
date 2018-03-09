3098cd9ec32f70847e6f74b37cd375ba1c6ca412
yves_2p_computer <yvesweissenberger@gmail.com>
2018-03-09 21:04:07 +0000

    data

--- Imaging_Training_MATLAB/monitor2.m
+++ Imaging_Training_MATLAB/monitor2.m
@@ -18,9 +18,9 @@ params = struct(...
     'edgeWin',0.01, ...        %size of cosine smoothing edge window in seconds
     'rewDur',0.1,...         %solenoid opening duration in seconds
     'maxRew',300, ...          %maximum number of rewards during experiment
-    'ISI_short_MEAN',5,...        %inter stimulus interval
+    'ISI_short_MEAN',5,...        %inter stimulus interval  %CHANGE ISI
     'ISI_STD',2,...
-    'ISI_long_MEAN',8,...        %inter stimulus interval
+    'ISI_long_MEAN',8,...        %inter stimulus interval  %LEGACY PARAMETER
     'maxDur',2700, ...          %maximum time of experiment in seconds
     'sndRewIntv',0.7 ...
     );
@@ -40,7 +40,7 @@ base = [folder 'Data' filesep];
 fTime = datestr(datetime('now','TimeZone','local'),'yyyymmdd-HHMMSS');
 subj = input('Type subject name: ','s');
 
-fName = ['Pretaining1_self_catch_trials_multilevel' subj '_' fTime '_data.txt'];
+fName = ['Pretaining1_self_catch_trials_' subj '_' fTime '_data.txt'];
 file_loc = strcat(base,fName);
 fileID = fopen(file_loc,'at+');
 %save(strcat(file_loc,'struct'), params)
@@ -125,34 +125,10 @@ click =  cat(2,zeros(1,500),ones(1,clickL),- ones(1,clickL),zeros(1,500))/2;
 trl_idx = 1;
 
 
-
-%this is early mapping starting levels, to gauge sensitivity
-%trl_order = Shuffle([0,2,3,4,5,6,99,0,2,3,4,5,6,99])';
-%trl_order = Shuffle([0,2,3,4,5,6,7,8,9,99,0,2,3,4,5,6,7,8,9,99])' ;
-trl_order = Shuffle([0,4,6,8,9,10,11,12,99,0,4,6,8,9,10,11,12,99])' ;
-
-
-%trl_order = cat(1,[0,0,6]',Shuffle([0,6,8,8.5,9,9.5,10,12,99,0,6,8,8.5,9,9.5,10,12,99,])');%diamond
-%trl_order = cat(1,[0,6]',Shuffle([0,6,7,8,8.5,9,9.5,10,10.5,11,99,0,6,7,8,8.5,9,9.5,10,10.5,11,99])');%biggie
-
-
-%easy one
-%trl_order = cat(1,[0,0,5]',Shuffle([0,4,4.5,5,5.5,6,6.5,7,9,11,99,0,4,4.5,5,6,6.5,7,9,11,99])');%biggie
+trl_order = Shuffle([2,2,2,2,1,1,1,1])';
 
 for i=1:50
-    %intial mapping
-    %trl_order = cat(1,trl_order,Shuffle([0,2,4,6,8,10,99,0,2,4,6,8,10,99])');
-    
-    %trl_order = cat(1,trl_order, Shuffle([0,2,3,4,5,6,99,0,2,3,4,5,6,99])');
-    %trl_order = cat(1,trl_order, Shuffle([0,1,1.25,1.5,1.75,2,2.25,99,0,1,1.25,1.5,1.75,2,2.25,99])');
-    trl_order = cat(1,trl_order, Shuffle([0,4,6,8,9,10,11,12,99,0,4,6,8,9,10,11,12,99])');
-
-    %trl_order = cat(1,trl_order,Shuffle([0,6,8,8.5,9,9.5,10,12,99,0,6,8,8.5,9,9.5,10,12,99])');
-    %trl_order = cat(1,trl_order,Shuffle([0,6,7,8,8.5,9,9.5,10,10.5,11,99,0,6,7,8,8.5,9,9.5,10,10.5,11,99])');    %%biggie and earlv
-     
-    %easy one
-    %trl_order = cat(1,trl_order,Shuffle([0,4,4.5,5,5.5,6,6.5,7,9,11,99,0,4,4.5,5,6,6.5,7,9,11,99])');    %%biggie and earlv
-
+    trl_order = cat(1,trl_order,Shuffle([2,2,2,2,1,1,1,1])');
 end
 while toc(tStart)<params.maxDur && rewCnt<params.maxRew
     
@@ -192,24 +168,21 @@ while toc(tStart)<params.maxDur && rewCnt<params.maxRew
         trl_idx = trl_idx + 1;
         curr_ISI = abs(normrnd(params.ISI_short_MEAN,params.ISI_STD)) + 3;
         
-        if rew_side<20
+        if rew_side==1
         
             %[snd, vol, frq] = get_stim(sndIdx,frqs,centreFreq,params);
             %The click is callibrated to ~70dB
 
-            vol = rew_side;
+            %vol = randi(5,1,1);
 
-         
-            snd = click/(1.584893^vol);
-
-            fprintf(strcat('__',num2str(vol),'__'))
+            snd = click;%click/(2^1);
             %PLAY SOUND
-            PsychPortAudio('FillBuffer', pahandle, snd);
-            PsychPortAudio('Start', pahandle);
+            %PsychPortAudio('FillBuffer', pahandle, snd);
+            %PsychPortAudio('Start', pahandle);
             sndT = toc(tStart);
 
 
-            fprintf(fileID,strcat('Sound:','click','_',num2str(vol),'_', ...
+            fprintf(fileID,strcat('Sound:','click','_',num2str(2), ...
                 num2str(sndT),'_', ...
                 num2str(frame_Nr),'\n'));
             
@@ -224,7 +197,7 @@ while toc(tStart)<params.maxDur && rewCnt<params.maxRew
             
             sndT = toc(tStart);
             fprintf(strcat('__',num2str(sndT)));
-            fprintf(fileID,strcat('Sound:','catch_trial','_',num2str(99),'_', ...
+            fprintf(fileID,strcat('Sound:','catch_trial','_',num2str(2), ...
                 num2str(toc(tStart)),'_', ...
                 num2str(frame_Nr),'\n'));
             
@@ -238,31 +211,31 @@ while toc(tStart)<params.maxDur && rewCnt<params.maxRew
     
         
         
-    if (hasplayed==true && hasLicked==true)
-        rew_mtx = [1,1];
-        fprintf(fileID, ...
-                strcat('rew:',num2str('RL'),'_', ...
-                num2str(toc(tStart)),'_', ... 
-                num2str(frame_Nr),'\n'));
-
-        outputSingleScan(s,rew_mtx);
-        fprintf('reward ')
-        rewT =  toc(tStart);
-        rewOn = true;
-        rewCnt = rewCnt + 2;
-        hasplayed = false;
-        collected = [0,0];
-        
-        
-    end
-    
-    
-    %STOP REWARD DELIVERY
-    if ( (toc(tStart)-rewT )>params.rewDur && rewOn )
-        outputSingleScan(s,[0,0]) %close solenoids
-        rew_mtx = [0,0];
-        rewOn = false;
-    end
+%     if (hasplayed==true && hasLicked==true)
+%         rew_mtx = [1,1];
+%         fprintf(fileID, ...
+%                 strcat('rew:',num2str('RL'),'_', ...
+%                 num2str(toc(tStart)),'_', ... 
+%                 num2str(frame_Nr),'\n'));
+% 
+%         %outputSingleScan(s,rew_mtx);
+%         %fprintf('reward ')
+%         rewT =  toc(tStart);
+%         rewOn = true;
+%         rewCnt = rewCnt + 2;
+%         hasplayed = false;
+%         collected = [0,0];
+%         
+%         
+%     end
+%     
+%     
+%     %STOP REWARD DELIVERY
+     if ( (toc(tStart)-rewT )>params.rewDur && rewOn )
+         outputSingleScan(s,[0,0]) %close solenoids
+         rew_mtx = [0,0];
+         rewOn = false;
+     end
     
     if (toc(tStart)-screenUpdateT)>2
         fprintf('\n');
